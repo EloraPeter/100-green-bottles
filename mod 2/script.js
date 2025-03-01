cb57bdb050907dfc812a0689cef95dcc0d070dd8
elorapeter <florenceonyi09@gmail.com>
2025-03-01 11:10:48 +0000

    Mar 1, 2025, 12:10 PM

--- mod 2/script.js
+++ mod 2/script.js
@@ -177,6 +177,7 @@ function startBottleStack() {
     console.log("Starting Bottle Stack game");
     bottleWall.classList.add("hidden");
     gameArea.classList.remove("hidden");
+    takeOneButton
     gameArea.innerHTML = `<h2>Build a Bottle Pyramid (5 at Base, 1 at Top)!</h2><div class="bottle-stack" id="stackArea"></div>`;
     numberOfBottles = 0; // Reset for stacking
     stars = 0;
